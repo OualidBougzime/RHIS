ae1f32248b835061a2c4a216b69f10c4e42972dd
OualidBougzime <55579475+OualidBougzime@users.noreply.github.com>
2021-10-25 21:24:42 -0400

    Update Deplacement.cs

--- RHIS Project/Scripts/Deplacement.cs
+++ RHIS Project/Scripts/Deplacement.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class Deplacement : MonoBehaviour
 {
-	
+	private Rigidbody2D rigidbody2D;
+
+    void Start(){
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        if(Input.GetKey(KeyCode.Q)){
+            rigidbody2D.velocity = new Vector2(-1, rigidbody2D.velocity.y);
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            rigidbody2D.velocity = new Vector2(1, rigidbody2D.velocity.y);
+        }
+
+        if(Input.GetKey(KeyCode.Z)){
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y, 1);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y, -1);
+        }
+    }
 }
