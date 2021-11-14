00f6169178da7c94eaf06e1e2c980a4450273257
OualidBougzime <55579475+OualidBougzime@users.noreply.github.com>
2021-11-13 22:59:27 -0500

    accueil + save and reload

---
+++ RHIS Project/Assets/Scripts/Save and reload/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level;
+    public int life;
+
+    public void SavePlayer(){
+    	SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer(){
+    	PlayerData data = SaveSystem.LoadPlayer();
+
+    	level = data.level;
+    	life = data.life;
+
+    	Vector3 position;
+    	position.x = data.position[0];
+    	position.y = data.position[1];
+    	position.z = data.position[2];
+    	transform.position = position;
+    }
+}
---
+++ RHIS Project/Assets/Scripts/ScriptsButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptsButton : MonoBehaviour
+{
+    public void Next()
+    {
+    	
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Next2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Back2()
+    {
+    	
+        SceneManager.LoadScene(2);
+    }
+    public void BackCercle2()
+    {
+    	
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
