afde7ad05ae73076b9dcd83b6e3868f6f0f1033f
Al-fayed TCHAGNAO <al-fayedtchagnao@MacBook-Pro-de-Al-fayed.local>
2021-11-22 19:37:43 -0500

    menu

---
+++ RHIS Project/Assets/Scripts/QualityManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualityManager : MonoBehaviour
+{
+    public void SetQuality(int qualityIndex){
+    	QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
