307d2ddb9c2d2bc356067a6f998d5be37a798fcc
Romain Razavet <razavet.r@gmail.com>
2021-11-30 19:31:16 -0500

    Setting DontDestroyOnLoad on Rhis, fixing null exception

--- RHIS Project/Assets/Scripts/Player/PlayerController.cs
+++ RHIS Project/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+	using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
 	public Animator anim;
 
 
+	
 
     // Start is called before the first frame update
     void Start()
@@ -91,7 +92,7 @@ public class PlayerController : MonoBehaviour
 	{
 		anim.SetTrigger("fire");
 
-		if (PlayerWeapon.AmmoInMagazine != 0)
+		if (PlayerWeapon != null && PlayerWeapon.AmmoInMagazine != 0)
 		{
 			StartCoroutine(PlayerWeapon.ShootWeapon());
 
@@ -107,8 +108,11 @@ public class PlayerController : MonoBehaviour
 
 	void reload()
 	{
-		//GetComponent<AudioSource>().PlayOneShot(SoundReload);
-		PlayerWeapon.ReloadWeapon();
+		if (PlayerWeapon != null)
+		{
+			//GetComponent<AudioSource>().PlayOneShot(SoundReload);
+			PlayerWeapon.ReloadWeapon();
+		}
 	}
 
 
