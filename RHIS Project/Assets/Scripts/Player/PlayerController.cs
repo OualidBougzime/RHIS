21c21b39522e9f061afb35925365507562107260
Romain Razavet <razavet.r@gmail.com>
2021-12-06 16:41:58 -0500

    Adding effects to weapon

--- RHIS Project/Assets/Scripts/Player/PlayerController.cs
+++ RHIS Project/Assets/Scripts/Player/PlayerController.cs
@@ -133,6 +133,24 @@ public class PlayerController : MonoBehaviour
 			//Bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.right) * Force;
 			//transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * Force, Space.World);
 			Destroy(bullet, 2f);
+
+			// Check if bullet has touched a enemy
+			if (PlayerWeapon.Effect == WeaponEffect.POISON)
+			{
+
+			} else if (PlayerWeapon.Effect == WeaponEffect.SUPER_POISON)
+			{
+				
+			} else if (PlayerWeapon.Effect == WeaponEffect.FIRE)
+			{
+				
+			} else if (PlayerWeapon.Effect == WeaponEffect.ICE)
+			{
+				
+			} else if (PlayerWeapon.Effect == WeaponEffect.RAGE)
+			{
+				
+			}
 		}
 		
 	}
